cb7166fbc1c05aa7b5719a498e8ae3a13ac24d1a
Josefo Koirala <josefokoirala@gmail.com>
2026-09-21 11:00:32 +0200

    Simplified version of isWeekend

--- src/IsWeekend.java
+++ src/IsWeekend.java
@@ -1,40 +1,49 @@
-/* Prøv at simplificere isWeekend-metoden */
+/* Try to simplify the isWeekend method */
 
 void main() {
     String dayOfWeek = "Mandag";
 
     boolean weekend = isWeekend(dayOfWeek);
-    IO.println("Er " + dayOfWeek + " i weekenden: " + weekend);
+    IO.println("Is " + dayOfWeek + " in Weekend: " + weekend);
 }
 
+//boolean isWeekend(String day) {
+//    boolean weekend = false;
+//
+//    switch (day) {
+//        case "Mandag":
+//            weekend = false;
+//            break;
+//        case "Tirsdag":
+//            weekend = false;
+//            break;
+//        case "Onsdag":
+//            weekend = false;
+//            break;
+//        case "Torsdag":
+//            weekend = false;
+//            break;
+//        case "Fredag":
+//            weekend = false;
+//            break;
+//        case "Lørdag":
+//            weekend = true;
+//            break;
+//        case "Søndag":
+//            weekend = true;
+//            break;
+//        default:
+//            weekend = false;
+//            break;
+//    }
+//    return weekend;
+//}
 boolean isWeekend(String day) {
-    boolean weekend = false;
-
     switch (day) {
-        case "Mandag":
-            weekend = false;
-            break;
-        case "Tirsdag":
-            weekend = false;
-            break;
-        case "Onsdag":
-            weekend = false;
-            break;
-        case "Torsdag":
-            weekend = false;
-            break;
-        case "Fredag":
-            weekend = false;
-            break;
         case "Lørdag":
-            weekend = true;
-            break;
         case "Søndag":
-            weekend = true;
-            break;
+            return true;
         default:
-            weekend = false;
-            break;
+            return false;
     }
-    return weekend;
 }
