cacf66a42ec923016bac6f224ddab72f92b6bb50
bertram <Bero100@stud.ek.dk>
2026-09-21 14:25:13 +0200

    Refactor duplicataes

--- src/DuplicateCondition.java
+++ src/DuplicateCondition.java
@@ -1,17 +1,13 @@
 /* Refaktorer koden for at fjerne duplikerede if-betingelser */
 
 void main() {
-    int age = 25;
+    int age = 17;
 
     if (age >= 18) {
         IO.println("Can vote");
-    }
-
-    if (age >= 18) {
-        IO.println("Can drive");
-    }
-
-    if (age >= 18) {
+        IO.println("Can Drive");
         IO.println("Is an adult");
+    } else {
+        IO.println("du kan intet");
     }
 }
