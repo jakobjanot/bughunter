7df50411d32c41cff5db388231b55b195870471b
emiltaangberg <taangberg.e@gmail.com>
2026-09-21 10:36:49 +0200

    Added parse to int age

--- src/AgeCheck.java
+++ src/AgeCheck.java
@@ -1,7 +1,7 @@
 /* Find fejlen i koden og ret den */
 
 void main() {
-    int age = IO.readln("Indtast din alder: ");
+    int age = Integer.parseInt(IO.readln("Indtast din alder: "));
     
     if (isAdult(age)) {
         IO.println("Du er en voksen.");
