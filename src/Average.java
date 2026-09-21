79e7114d1d1b7a4707636a19b8706e83dda22722
Josefo Koirala <josefokoirala@gmail.com>
2026-09-21 10:52:03 +0200

    Average file has been updated

--- src/Average.java
+++ src/Average.java
@@ -1,18 +1,32 @@
-/* Find ud hvordan vi håndterer tomme lister */
+/* Find out how we handle empty lists */
 
 void main() {
-    IO.println("Gennemsnittet af 1, 2, 3, 4, 5 er: " 
+    IO.println("The average of 1, 2, 3, 4, 5 is: "
         + calculateAverage(new int[]{1, 2, 3, 4, 5}));
-    IO.println("Gennemsnittet af en tom liste er: " 
+    IO.println("Average of the empty list is: "
         + calculateAverage(new int[]{}));
 }
 
+//double calculateAverage(int[] numbers) {
+//    int sum = 0;
+//
+//    for (int number : numbers) {
+//        sum += number;
+//    }
+//
+//    return  sum / numbers.length;
 double calculateAverage(int[] numbers) {
+    if (numbers.length == 0) {
+        return 0;
+    }
+
     int sum = 0;
 
     for (int number : numbers) {
         sum += number;
     }
 
-    return  sum / numbers.length;
+    return (double) sum / numbers.length;
 }
+
+
