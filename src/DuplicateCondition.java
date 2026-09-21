ebee04b86668d5b060a08081d1e07872be383e28
godfdestroyer <malthelentzjonasson@gmail.com>
2026-09-21 14:52:31 +0200

    made it one if with an else

--- src/DuplicateCondition.java
+++ src/DuplicateCondition.java
@@ -2,16 +2,14 @@
 
 void main() {
     int age = 25;
-
+IO.println("is "+age+" years old");
     if (age >= 18) {
         IO.println("Can vote");
-    }
-
-    if (age >= 18) {
-        IO.println("Can drive");
-    }
-
-    if (age >= 18) {
         IO.println("Is an adult");
+        IO.println("Can drive");
+    }else {
+        IO.println("Cant vote");
+        IO.println("Is not an adult");
+        IO.println("Can not legaly drive");
     }
 }
